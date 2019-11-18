9910cab2e050db184b99d225b318b276b59f8dee
Maddl1994 <57004164+Maddl1994@users.noreply.github.com>
2019-11-18 12:45:30 +0100

    Neue Dateien vom Michi erstellt

---
+++ src/servlets/Logout.java
@@ -0,0 +1,38 @@
+// Quelle für die invalidate Funktion : https://tomcat.apache.org/tomcat-5.5-doc/servletapi/javax/servlet/http/HttpSession.html
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		session.invalidate();
+		request.getRequestDispatcher("html/logout.jsp").include(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/servlets/Password.java
@@ -0,0 +1,95 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import beans.User;
+
+
+/**
+ * Servlet implementation class passwordServlet
+ */
+@WebServlet(name = "Password")
+public class Password extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Resource(lookup ="java:jboss/datasources/MySqlGlobal11DS")
+	private DataSource ds;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String newPassword = request.getParameter("newPassword");
+		String passwordConfirmation = request.getParameter("passwordConfirmation");
+		
+		// Benutzerbean laden
+		HttpSession session = request.getSession();
+		User user = (User) session.getAttribute("user");
+		String password = user.getPassword();
+
+		// Fehlerfälle überprüfen
+		boolean error = false;
+		
+		// Felder gefüllt?
+		if (newPassword == null || newPassword == "" || passwordConfirmation == null || passwordConfirmation == "" || password == "" || password == null)
+			error = true;
+		else {
+		
+			// Stimmen neues Passwort und Passwortbestätigung überein?
+			if (!newPassword.equals(passwordConfirmation))
+				error = true;
+			
+			// Neues und altes Passwort müssen unterschiedlich sein
+			if (newPassword.equals(user.getPassword()))
+				error = true;
+			// Das Passwort, welches in der Bean gespeichert ist, muss mit den aktuellen Passwort übereinstimmen
+			// wenn nein dann error
+		
+		}
+		
+		// Fehler entdeckt? Falls ja, Umleitung zur Fehlerseite
+		if (error) {
+			final RequestDispatcher dispatcher = request.getRequestDispatcher("html/welcomeUser.jsp");
+			dispatcher.forward(request, response);
+			return;
+		}else if((password.equals(request.getParameter("oldPassword"))))
+		
+		// Kein Fehler entdeckt: DB-Zugriff
+		try(Connection conn = ds.getConnection(); 
+			PreparedStatement pstm = conn.prepareStatement("UPDATE user"
+					+ " SET password = ?"
+					+ "WHERE email = ?");){
+
+			pstm.setString(1,newPassword);
+			pstm.setString(2, user.getEmail());
+			pstm.executeUpdate();
+			conn.close();
+		}
+		catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		
+		final RequestDispatcher dispatcher = request.getRequestDispatcher("html/passwordAenderung.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
