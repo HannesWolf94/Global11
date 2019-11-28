798c432859876b187852d4a81224ab8bcaf6e67b
MichaelHaid <michi.haid@gmx.de>
2019-11-28 14:20:09 +0100

    Produkt löschen anhand Label

--- src/servlets/ProductDelete.java
+++ src/servlets/ProductDelete.java
@@ -1,39 +1,71 @@
+// Maddl, Michi
 package servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
 
-/**
- * Servlet implementation class ProductDelete
- */
-@WebServlet("/ProductDelete")
+import beans.Product;
+
+
+@WebServlet("ProductDelete")
 public class ProductDelete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProductDelete() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Resource(lookup = "java:jboss/datasources/MySqlGlobal11DS")
+	private DataSource ds;
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8"); // In diesem Format erwartet das Servlet jetzt die Formulardaten
+		String label = request.getParameter("label");
+		String labelRepeat = request.getParameter("labelRepeat");
+		
+		if ((labelCheck(label, labelRepeat) == true)) {
+			delete(label);
+
+	        final RequestDispatcher dispatcher = request.getRequestDispatcher("ProductAnzeigen");
+	        dispatcher.forward(request, response);
+        } else {
+//        	Fehlermeldung.jsp oder die Eingabefelder rot umranden wenn email nicht übereinstimmt 
+        	final RequestDispatcher dispatcher = request.getRequestDispatcher("html/impressum.jsp");
+             dispatcher.forward(request, response);
+        }
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void delete(String label) throws ServletException {
+		Product product = new Product(); 
+		product.setLabel(label); 
+		
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement("DELETE FROM product WHERE prod_label = ?;")) {
+			pstmt.setString(1, label);
+			pstmt.executeUpdate();
+		} catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
+	}
+	
+	protected boolean labelCheck(String label, String labelRepeat) throws ServletException {
+		if (label.equals(labelRepeat)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
