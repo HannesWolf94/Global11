325ba8b1372bf961409c6b87d4bb3de1cda39da3
Maddl1994 <57004164+Maddl1994@users.noreply.github.com>
2019-12-13 11:02:55 +0100

    Suche angepasst an neue rs.next

--- src/servlets/Suche.java
+++ src/servlets/Suche.java
@@ -19,7 +19,6 @@ import javax.sql.DataSource;
 
 import beans.Product;
 
-
 @WebServlet("/Suche")
 public class Suche extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -27,75 +26,67 @@ public class Suche extends HttpServlet {
 	@Resource(lookup = "java:jboss/datasources/MySqlGlobal11DS")
 	private DataSource ds;
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Servlet zur Entgegennahme von Formularinhalten, Lesen der Daten in einer DB und Generierung
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Servlet zur Entgegennahme von Formularinhalten, Lesen der Daten in einer DB
+		// und Generierung
 		// eines Beans zur Weitergabe der Formulardaten an eine JSP
-		
-		request.setCharacterEncoding("UTF-8");	// In diesem Format erwartet das Servlet jetzt die Formulardaten
+
+		request.setCharacterEncoding("UTF-8"); // In diesem Format erwartet das Servlet jetzt die Formulardaten
 		String kategorie = request.getParameter("kategorie");
 		String label = request.getParameter("label");
-		
+
 		// DB-Zugriff
 		List<Product> products = search(kategorie, label);
-				
+
 		// Scope "Request"
 		request.setAttribute("products", products);
-		
+
 		// Weiterleiten an JSP
 		final RequestDispatcher dispatcher = request.getRequestDispatcher("UserProductGesamt");
-		dispatcher.forward(request, response);	
+		dispatcher.forward(request, response);
 	}
 
 	private List<Product> search(String kategorie, String label) throws ServletException {
 		kategorie = (kategorie == null || kategorie == "") ? "%" : "%" + kategorie + "%";
 		label = (label == null || label == "") ? "%" : "%" + label + "%";
 		List<Product> products = new ArrayList<Product>();
-		
+
 		// DB-Zugriff
 		try (Connection con = ds.getConnection();
-			 PreparedStatement pstmt = con.prepareStatement("SELECT * FROM product WHERE cat_description like ? and prod_label LIKE ?")) {
+				PreparedStatement pstmt = con
+						.prepareStatement("SELECT * FROM product WHERE cat_description like ? and prod_label LIKE ?")) {
 
 			pstmt.setString(1, kategorie);
 			pstmt.setString(2, label);
 			try (ResultSet rs = pstmt.executeQuery()) {
-			
+
 				while (rs.next()) {
 					Product product = new Product();
-					
-					Integer prodId = Integer.valueOf(rs.getInt("prod_id"));
-					product.setProdId(prodId);
-					
-					String prod_kategorie = rs.getString("cat_description");
-					product.setKategorie(prod_kategorie);
-					
-					String prodLabel = rs.getString("prod_label");
-					product.setLabel(prodLabel);
-					
-					String type = rs.getString("prod_type");
-					product.setType(type);
-					
-					String colour = rs.getString("prod_colour");
-					product.setColour(colour);
-					
-					
-					Double price = Double.valueOf(rs.getDouble("prod_price"));
-					product.setPrice(price);
-					
+
+					product.setProdId(rs.getInt("prod_id"));
+					product.setKategorie(rs.getString("cat_description"));
+					product.setLabel(rs.getString("prod_label"));
+					product.setType(rs.getString("prod_type"));
+					product.setColour(rs.getString("prod_colour"));
+					product.setPrice(rs.getDouble("prod_price"));
+
 					products.add(product);
 				}
 			}
 		} catch (Exception ex) {
 			throw new ServletException(ex.getMessage());
 		}
-		
+
 		return products;
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
